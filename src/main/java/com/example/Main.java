acddf0e243b999e2537d45b6eb377d1a5d7661a9
Adarsh-git02 <guptaadarsh628@gmail.com>
2024-07-08 00:12:32 +0530

    Changes on branch b2

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Software Engg Lab");
         System.out.println("Software Engg Lab on Github");
         System.out.println("Changes in branch b1");
+        System.out.println("Changes in branch b2");
     }
 }
